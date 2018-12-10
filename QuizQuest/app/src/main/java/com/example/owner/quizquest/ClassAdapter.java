765f85d44647ef4e76da1d98a9d3bfe4f0e4fa00
ChrisBratti <42649073+ChrisBratti@users.noreply.github.com>
2018-12-09 21:56:40 -0500

    Added the ability to add nicknames to classes
    Added the ability to add nicknames to classes. However, since the database has no way to store nicknames right now, the nicknames aren't stored and are temporary. If the user navigates away from the screen, the name dissapaears and will need to be added back.
    Also removed the delete button, as users shouldn't be able to delete classes with only the press of a button

--- QuizQuest/app/src/main/java/com/example/owner/quizquest/ClassAdapter.java
+++ QuizQuest/app/src/main/java/com/example/owner/quizquest/ClassAdapter.java
@@ -1,14 +1,24 @@
 package com.example.owner.quizquest;
 
+import android.app.ActionBar;
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -25,9 +35,12 @@ Homework 07
 
 public class ClassAdapter extends ArrayAdapter<Class> {
     private List<Class> classes;
+    Context context;
+    private String nickname;
     public ClassAdapter(@NonNull Context context, @LayoutRes int resource, @NonNull List<Class> objects){
         super(context, resource, objects);
         this.classes = objects;
+        this.context = context;
     }
 
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent){
@@ -43,22 +56,25 @@ public class ClassAdapter extends ArrayAdapter<Class> {
             viewHolder.nickName = convertView.findViewById(R.id.tvClassName);
             viewHolder.teacherName = convertView.findViewById(R.id.tvTeacherName);
             viewHolder.className = convertView.findViewById(R.id.tvClassNumber);
-            viewHolder.delete = convertView.findViewById(R.id.imgBtnDelete);
             viewHolder.edit = convertView.findViewById(R.id.imgBtnEditClass);
             convertView.setTag(viewHolder);
         }else{
             viewHolder = (ViewHolder) convertView.getTag();
         }
+        if(aClass.getNickName() == null){
+            viewHolder.nickName.setVisibility(View.INVISIBLE);
+        }else{
+            viewHolder.nickName.setText(aClass.getNickName());
+        }
 
-        viewHolder.nickName.setText(aClass.getNickName());
         viewHolder.className.setText(aClass.getName());
         viewHolder.teacherName.setText(aClass.getTeacher());
 
-        viewHolder.delete.setOnClickListener(new View.OnClickListener() {
+
+        viewHolder.edit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                classes.remove(aClass);
-                ClassAdapter.this.notifyDataSetChanged();
+                showPopup(v, aClass);
             }
         });
 
@@ -72,11 +88,70 @@ public class ClassAdapter extends ArrayAdapter<Class> {
         TextView className;
         TextView teacherName;
         TextView nickName;
-        ImageButton delete;
         ImageButton edit;
 
     }
 
+    public void showPopup(View anchorView, final Class aClass){
+        nickname = null;
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService( Context.LAYOUT_INFLATER_SERVICE );
+        View popupView  = inflater.inflate(R.layout.layout_edit_class, null);
+        final PopupWindow popupWindow = new PopupWindow(popupView, ActionBar.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        final EditText classNickname = popupView.findViewById(R.id.etClassNickname);
+        Button cancel = popupView.findViewById(R.id.btnNicknameCancel);
+        Button addNickname = popupView.findViewById(R.id.btnAddNickname);
+
+
+        popupWindow.setFocusable(true);
+        popupWindow.update();
+
+
+        int strokeWidth = 5; // 5px not dp
+        int roundRadius = 15; // 15px not dp
+        int strokeColor = Color.parseColor("#2E3135");
+        int fillColor = getPrimaryColor();
+
+        GradientDrawable gd = new GradientDrawable();
+        gd.setColor(fillColor);
+        gd.setCornerRadius(roundRadius);
+        gd.setStroke(strokeWidth, strokeColor);
+
+        popupWindow.setBackgroundDrawable(gd);
+
+        int location[] = new int[2];
+        anchorView.getLocationOnScreen(location);
+
+        popupWindow.showAtLocation(anchorView, Gravity.NO_GRAVITY, location[0], location[1] + anchorView.getHeight());
+
+        addNickname.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Test", "Add class was clicked");
+                nickname = classNickname.getText().toString();
+                aClass.setNickName(nickname);
+                ClassAdapter.this.notifyDataSetChanged();
+                popupWindow.dismiss();
+            }
+        });
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Test", "Cancel was clicked");
+                popupWindow.dismiss();
+            }
+        });
+    }
+
+    private int getPrimaryColor(){
+        TypedValue typedValue = new TypedValue();
+
+        TypedArray a = getContext().obtainStyledAttributes(typedValue.data, new int[] {R.attr.colorPrimary});
+        int color = a.getColor(0,0);
+        a.recycle();
+        return color;
+    }
 
 
 
